5dfcbca2d4e6b8f103becf6543b3c253dd57045d
Jamie Little <jamie@jamielittle.org>
2018-09-18 21:31:41 +0000

    Initial commit
    This is the intial commit for the Ursus Blacklight
    application. It was generated using the instructions
    found at:
    
    https://github.com/projectblacklight/blacklight/wiki/Quickstart#creating-a-new-application-the-hard-way
    
    Connected to #23

---
+++ config/routes.rb
@@ -0,0 +1,42 @@
+Rails.application.routes.draw do
+  
+  
+    concern :searchable, Blacklight::Routes::Searchable.new
+
+  resource :catalog, only: [:index], as: 'catalog', path: '/catalog', controller: 'catalog' do
+    concerns :searchable
+  end
+
+  devise_for :users
+  concern :exportable, Blacklight::Routes::Exportable.new
+
+  resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
+    concerns :exportable
+  end
+
+  resources :bookmarks do
+    concerns :exportable
+
+    collection do
+      delete 'clear'
+    end
+  end
+
+  mount Blacklight::Engine => '/'
+  root to: "catalog#index"
+  concern :exportable, Blacklight::Routes::Exportable.new
+
+  resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
+    concerns :exportable
+  end
+
+  resources :bookmarks do
+    concerns :exportable
+
+    collection do
+      delete 'clear'
+    end
+  end
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
