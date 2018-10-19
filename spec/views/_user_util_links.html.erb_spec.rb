c9a88c5817897643e64c1e92adfafdd1cbd241eb
Jamie Little <jamie@jamielittle.org>
2018-10-19 15:17:50 -0500

    Add specs for turning off user login display
    This adds view specs that cover cases where
    the login links are turned on or off via
    configuration. It also updates the path
    in the views to the configuation by adding
    `Rails.application` when accessing the property.
    This was needed when running the specs.

--- spec/views/_user_util_links.html.erb_spec.rb
+++ spec/views/_user_util_links.html.erb_spec.rb
@@ -2,27 +2,23 @@
 
 require 'rails_helper'
 
-RSpec.describe 'catalog/_bookmark_control.html.erb' do
-  let(:document) { SolrDocument.new id: 'xyz', format: 'a' }
+RSpec.describe '_user_util_links.html.erb' do
   let(:blacklight_config) { Blacklight::Configuration.new }
 
   before do
-    assign :document, document
     allow(view).to receive(:blacklight_config).and_return blacklight_config
-    allow(view).to receive(:document) { document }
-    allow(view).to receive(:documents) { [document] }
-    allow(view).to receive(:bookmarked?) { false }
+    allow(view).to receive(:has_user_authentication_provider?).and_return(true)
   end
 
   it 'does not display if turned off' do
     Rails.application.config.user_account_ui_enabled = 'false'
     render
-    expect(rendered).to eq('')
+    expect(rendered).not_to match(/Login/)
   end
 
   it 'displays if it is turned on' do
     Rails.application.config.user_account_ui_enabled = 'true'
     render
-    expect(rendered).to match(/bookmark-toggle/)
+    expect(rendered).to match(/Login/)
   end
 end
