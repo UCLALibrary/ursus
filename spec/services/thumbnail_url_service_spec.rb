264fcdffb9c4067109614d9f9bf3819fcd829f7d
Jamie Little <jamie@jamielittle.org>
2018-10-23 17:21:34 -0500

    Add spec for thumbnail service

---
+++ spec/services/thumbnail_url_service_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ThumbnailUrlService do
+
+  let(:html) { '<a href="test"><img src="/something" /></a>' }
+  let(:tn_url_service) { described_class.new(url: 'http://example.com/', html: html) }
+
+  it 'can append a string to the src attribute' do
+    expect(tn_url_service.markup_with_full_url).to match(/http:\/\/example.com/)
+  end
+end
