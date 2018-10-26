9b79f38f8875a66bb0b64c64ff05dd0a01abfb06
val99erie <val99erie@gmail.com>
2018-10-26 14:55:56 -0500

    Add feature spec to test search results page.
    * Add a spec helper method to delete all documents from solr.
    
    * Change the 'search' requestHandler config to exactly match the config
    in californica.  With the old config, the spec failed because blacklight
    used the ID instead of the title to construct the document link.  That
    happened because only a partial solr doc was returned with the old
    config, so the title wasn't available to create the link.

---
+++ spec/support/solr_helpers.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module SolrHelpers
+  def delete_all_documents_from_solr
+    solr = Blacklight.default_index.connection
+    solr.delete_by_query('*:*')
+    solr.commit
+  end
+end
