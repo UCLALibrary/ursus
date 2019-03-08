d9494958a53481b795fc39d9b6650b83b38ff3e9
Andrew Wallace <andrewwallace@library.ucla.edu>
2019-03-08 15:28:39 -0800

    fix: citation bugs - placeholder text when a field is missing - build URL off ENV['RAILS_HOST'], not 'ursus-test' - use dlcs_collection_name_ssm instead of publisher_tesim

---
+++ spec/models/solr_document_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+ENV['RAILS_HOST'] ||= 'ursus-test'
+
+RSpec.describe SolrDocument do
+  let(:solr_document) do
+    described_class.new(id: 'abc123',
+                        title_tesim: ['Test record'],
+                        dlcs_collection_name_ssm: ['Collection 1'],
+                        resource_type_tesim: ['still image'])
+  end
+
+  it 'formats a citation' do
+    expect(solr_document.export_as_ucla_citation_txt).to eq("Test record. [still image]. UCLA Library Digital Collections. Collection 1. https://#{ENV['RAILS_HOST']}/catalog/abc123")
+  end
+
+  context 'when fields are missing' do
+    let(:solr_document) do
+      described_class.new(id: 'abc123',
+                          title_tesim: [],
+                          dlcs_collection_name_ssm: [],
+                          resource_type_tesim: [])
+    end
+
+    it 'uses placeholder values' do
+      expect(solr_document.export_as_ucla_citation_txt).to eq("Untitled. [unknown type]. UCLA Library Digital Collections. No collection. https://#{ENV['RAILS_HOST']}/catalog/abc123")
+    end
+  end
+end
