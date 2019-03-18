07d3b82ee0d8fb42b9116c67945c586f3578edc0
jendiamond <thejendiamond@gmail.com>
2019-03-18 10:21:01 -0700

    Prevent searches from being saved to the database
    Currently we run a rake task everyday that deletes the searches
    
    **`config/schedule.rb:25`**
    ```
    rake "blacklight:delete_old_searches[1]"
    ```
    
    ---
    
    branch do-not-save-searches_287
    Changes to be committed:
    + modified:   app/controllers/catalog_controller.rb
    + new file:   spec/features/search_crawler_spec.rb

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -23,6 +23,9 @@ class CatalogController < ApplicationController
     ## Model that maps search index responses to the blacklight response model
     # config.response_model = Blacklight::Solr::Response
 
+    # Do not store searches for anyone since they are not displayed and will fill up the database
+    config.crawler_detector = ->(req) { true }
+
     ## Default parameters to send to solr for all search-like requests. See also SearchBuilder#processed_parameters
     config.default_solr_params = {
       qt: 'search',
