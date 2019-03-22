cb696de2271dff9e01dbdeef1e349e72f42351c1
Jamie Little <jamie@jamielittle.org>
2019-03-22 12:37:39 -0500

    Add a static about page
    This introduces an about page that
    you can view at `/about` based on the
    c5 design.
    
    Connected to UCLALibrary/ursus#269

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,5 +63,6 @@ RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
 
+
   WebMock.allow_net_connect!
 end
