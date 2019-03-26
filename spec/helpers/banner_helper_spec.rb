d51b2f16f024708c07c8dec19aea3f9c07d5ff9d
Jamie Little <jamie@jamielittle.org>
2019-03-26 16:37:47 -0500

    Better Collection Banner styling
    This improves the styling of the collection banner.
    It's based on the CSS that is already in californica/hyrax.

--- spec/helpers/banner_helper_spec.rb
+++ spec/helpers/banner_helper_spec.rb
@@ -16,6 +16,17 @@ RSpec.describe BannerHelper, type: :helper do
       ).to_return(status: 200, body: bod,
                   headers: { 'Content-Type' => 'application/json' })
   end
+  let(:local_stubbed_request) do
+    stub_request(:get, 'http://localhost:3000/branding_info/1.json')
+      .with(
+        headers: {
+          'Accept' => '*/*',
+          'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent' => 'Ruby'
+        }
+      ).to_return(status: 200, body: bod,
+                  headers: { 'Content-Type' => 'application/json' })
+  end
 
   before do
     WebMock.disable_net_connect!
@@ -42,21 +53,25 @@ RSpec.describe BannerHelper, type: :helper do
       doc['id'] = '1'
       doc['has_model_ssim'] = ['Collection']
       stubbed_request
-      doc['has_model_ssim'] = ['Collection']
-      expect(render_banner_tag(doc)).to eq("<img width='800' alt='Collection banner' height='100' src='http://californica-test.library.ucla.edu/branding/3049304/banner/test.tif'/>")
+      local_stubbed_request
+      expect(render_banner_tag(doc)).to be_in(["<div class='collection-banner' alt='Collection banner' style='background-image:url(http://localhost:3000/branding/3049304/banner/test.tif);'></div>",
+                                               "<div class='collection-banner' alt='Collection banner' style='background-image:url(http://californica-test.library.ucla.edu/branding/3049304/banner/test.tif);'></div>"]) # rubocop:disable Metrics/LineLength
     end
   end
 
   describe '#get_img_path' do
     it 'returns the url to the image' do
       stubbed_request
-      expect(get_img_path('1')).to eq('http://californica-test.library.ucla.edu/branding/3049304/banner/test.tif')
+      local_stubbed_request
+      expect(get_img_path('1')).to be_in(['http://californica-test.library.ucla.edu/branding/3049304/banner/test.tif',
+                                          'http://localhost:3000/branding/3049304/banner/test.tif'])
     end
   end
 
   describe '#get_branding_info' do
     it 'returns an array with the path to the branding info' do
       stubbed_request
+      local_stubbed_request
       expect(get_branding_info('1')).to eq(['/opt/path',
                                             'branding/3049304/banner/test.tif'])
     end
