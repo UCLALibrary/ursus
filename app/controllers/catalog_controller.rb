11644338ce258f88af08e1c3b211de19781d3584
Jamie Little <jamie@jamielittle.org>
2019-03-26 11:44:14 -0500

    Rubocop fixes
    Fixes for some changes introduced by `blacklight-gallery`
    installation.

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -18,7 +18,6 @@ class CatalogController < ApplicationController
     config.view.masonry.partials = [:index]
     config.view.slideshow.partials = [:index]
 
-
     config.show.tile_source_field = :content_metadata_image_iiif_info_ssm
     config.show.partials.insert(1, :openseadragon)
     ## Class for sending and receiving requests from a search index
