57ab06de5e2c08e90fe11e9e9eaf9c4cf34e997f
jendiamond <thejendiamond@gmail.com>
2019-03-27 12:55:24 -0700

    Update search results list view to C5 design (v3)
    Connected to
    
    - [x] There is a header that has a result count: like  3 Catalog Results in the
    - [x] Add labels to the view buttons
    - [x] Reduce the size of the title header  in the search results (number is the same size as well)
    - [x] Metadata field label are dark blue and bold
    - [x] Title has larger width than Description
    
    ---
    
    Changes to be committed:
    + modified:   app/assets/stylesheets/ursus.scss
    + modified:   app/assets/stylesheets/ursus/_fonts.scss
    + new file:   app/assets/stylesheets/ursus/_title.scss
    + modified:   app/assets/stylesheets/ursus/_variables.scss
    + modified:   app/views/catalog/_search_results.html.erb
    + new file:   app/views/catalog/_view_type_group.html.erb
    + new file:   app/views/catalog/_document.html.erb
    + new file:   app/views/catalog/_index_header.html.erb
    + new file:   app/views/catalog/_paginate_compact.html.erb

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -15,8 +15,8 @@ class CatalogController < ApplicationController
 
   configure_blacklight do |config|
     config.view.gallery.partials = [:index_header, :index]
-    config.view.masonry.partials = [:index]
-    config.view.slideshow.partials = [:index]
+    # config.view.masonry.partials = [:index]
+    # config.view.slideshow.partials = [:index]
 
     config.show.tile_source_field = :content_metadata_image_iiif_info_ssm
     config.show.partials.insert(1, :openseadragon)
