026e71d648b0add0258c5a79e32e0199bb385d1a
jendiamond <thejendiamond@gmail.com>
2019-06-06 14:35:14 -0700

    Add 'alternative_title' field CAL-584
    This is the Ursus component of CAL-584
    
    ---
    
    + modified:   app/controllers/catalog_controller.rb
    + modified:   app/services/search_field_service.rb
    + modified:   spec/services/search_field_service_spec.rb
    + modified:   spec/support/works.rb

--- spec/services/search_field_service_spec.rb
+++ spec/services/search_field_service_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe SearchFieldService do
   let(:search_field_service) { described_class.instance }
 
   it 'returns the correct list of search fields' do
-    expect(search_field_service.search_fields).to eq(%w[ architect_tesim caption_tesim
+    expect(search_field_service.search_fields).to eq(%w[ alternative_title_tesim architect_tesim caption_tesim
                                                          contributor_tesim creator_tesim description_tesim genre_tesim
                                                          identifier_tesim local_identifier_ssm location_tesim medium_tesim
                                                          named_subject_tesim normalized_date_tesim
