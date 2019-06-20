3b3f15b9255f5b0e05601471ed2e99f39410695e
Andrew Wallace <andrewwallace@library.ucla.edu>
2019-06-20 13:30:51 -0700

    389_add-google-analytics
    Connected to [URS-389](https://jira.library.ucla.edu/browse/URS-389)
    
    Add Google analytics to Ursus production
    
    ---
    
    + modified: `app/views/layouts/blacklight/base.html.erb`
    + new file: `spec/system/view_home_page_spec.rb`

---
+++ spec/system/view_home_page_spec.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'viewing the home page', :clean, type: :system, js: false do
+  it 'has a 200 status code' do
+    visit('/')
+    expect page.status_code == 200
+  end
+
+  it 'has h1' do
+    visit('/')
+    expect(page).to have_selector 'h1', text: 'UCLA Library Digital Collections'
+  end
+
+  it 'has an About link' do
+    visit('/')
+    expect(page).to have_link 'About'
+  end
+
+  it 'has an feedback link' do
+    visit('/')
+    expect(page).to have_link 'Give us feedback'
+  end
+
+  it 'has a copyright & collections link' do
+    visit('/')
+    expect(page).to have_link 'Copyright and Collections'
+  end
+
+  it 'has a privacy policy link' do
+    visit('/')
+    expect(page).to have_link 'Collections'
+  end
+
+  it 'has a contact link' do
+    visit('/')
+    expect(page).to have_link 'Contact'
+  end
+
+  it 'doesn\'t have Google Analytics when it shouldn\'t' do
+    visit('/')
+    expect(page).not_to have_selector 'script#analytics-script'
+    expect(page).not_to have_selector 'noscript#analytics-noscript'
+  end
+
+  it 'has Google Analytics when it should' do
+    ENV['GOOGLE_TAG_MGR_ID'] = '123456789'
+    visit('/')
+    expect(page).to have_selector '#analytics-script'
+    expect(page).to have_selector '#analytics-noscript'
+    ENV.delete('GOOGLE_TAG_MGR_ID')
+  end
+end
