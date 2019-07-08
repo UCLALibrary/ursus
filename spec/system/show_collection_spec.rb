eac5e094f0484d8e605ae8d70e187331790b3162
jendiamond <thejendiamond@gmail.com>
2019-07-08 15:32:55 -0700

    Refactor the catalog controller
    Connected to [URS-441](https://jira.library.ucla.edu/browse/URS-441)
    
    Refactored for better clarity
    
    Changed from:
    `::Solrizer.solr_name('funding_note', :stored_searchable)`
    
    To:
    `'funding_note_tesim'`
    
    ---
    
    Changes to be committed:
    + modified: `app/controllers/catalog_controller.rb`
    + modified: `spec/system/genre_display_spec.rb`
    + new file: `spec/system/photographer_display_spec.rb`
    + renamed:  `spec/system/search_collection_results.rb -> spec/system/search_collection_results_spec.rb`
    + modified: `spec/system/show_collection_spec.rb`
    + new file: `spec/system/subject_display_spec.rb`

--- spec/system/show_collection_spec.rb
+++ spec/system/show_collection_spec.rb
@@ -3,62 +3,38 @@
 require 'rails_helper'
 
 RSpec.describe 'Search results page', type: :system, js: true do
-  let(:id) { "m8f11000zz-89112" }
+  let(:id) { 'm8f11000zz-89112' }
   let(:services_contact) do
-    "UCLA Charles E. Young Research Library Department of Special Collections Phone: (310)825-4988"
+    'UCLA Charles E. Young Research Library Department of Special Collections Phone: (310)825-4988'
   end
   let(:description) do
-    "Walter E. Bennett (1921-1995) was the first salaried photographer for Time, where he worked from 1952 to 1982."
+    'Walter E. Bennett (1921-1995) was the first salaried photographer for Time, where he worked from 1952 to 1982.'
   end
   let(:collection_attributes) do
     {
       id: id,
-      has_model_ssim: [
-        "Collection"
-      ],
-      accessControl_ssim: [
-        "7b1af782-af1f-46a6-9bd2-b53be0f1bb68"
-      ],
-      title_tesim: [
-        "Bennett (Walter E.) Photographic Collection, 1937-1983 (bulk 1952-1982)"
-      ],
-      collection_type_gid_ssim: [
-        "gid://californica/hyrax-collectiontype/1"
-      ],
-      ark_ssi: "ark:/21198/zz00011f8m",
-      local_identifier_ssm: [
-        "Collection 686"
-      ],
-      normalized_date_tesim: [
-        "1937/1983"
-      ],
-      photographer_tesim: [
-        "Bennett, Walter E. (Walter Edward), 1921-1995"
-      ],
-      repository_tesim: [
-        "University of California, Los Angeles. Library Special Collections"
-      ],
-      services_contact_ssm: [services_contact],
-      human_readable_resource_type_tesim: [
-        "still image"
-      ],
-      description_tesim: [
-        description
-      ],
-      rights_statement_tesim: [
-        "http://vocabs.library.ucla.edu/rights/copyrighted"
-      ],
-      date_created_tesim: [
-        "1937-1983"
-      ],
-      thumbnail_path_ss: "/assets/collection-a38b932554788aa578debf2319e8c4ba8a7db06b3ba57ecda1391a548a4b6e0a.png",
-      visibility_ssi: "open",
-      read_access_group_ssim: ["public"],
-      ursus_id_ssi: "21198-zz00011f8m",
-      human_readable_type_tesim: [
-        "Collection"
-      ],
-      license_tesim: ['https://creativecommons.org/licenses/by/4.0/']
+      has_model_ssim: ['Collection'],
+      accessControl_ssim: ['7b1af782-af1f-46a6-9bd2-b53be0f1bb68'],
+      title_tesim: ['Bennett (Walter E.) Photographic Collection, 1937-1983 (bulk 1952-1982)'],
+      collection_type_gid_ssim: ['gid://californica/hyrax-collectiontype/1'],
+      ark_ssi: 'ark:/21198/zz00011f8m',
+      local_identifier_ssm: ['Collection 686'],
+      normalized_date_tesim: ['1937/1983'],
+      photographer_tesim: ['Bennett, Walter E. (Walter Edward), 1921-1995'],
+      repository_tesim: ['University of California, Los Angeles. Library Special Collections'],
+      services_contact_ssm: ['UCLA'],
+      human_readable_resource_type_tesim: ['still image'],
+      description_tesim: ['description'],
+      rights_statement_tesim: ['http://vocabs.library.ucla.edu/rights/copyrighted'],
+      date_created_tesim: ['1937-1983'],
+      extent_tesim: ['still image'],
+      thumbnail_path_ss: '/assets/collection-a38b932554788aa578debf2319e8c4ba8a7db06b3ba57ecda1391a548a4b6e0a.png',
+      visibility_ssi: 'open',
+      read_access_group_ssim: ['public'],
+      ursus_id_ssi: '21198-zz00011f8m',
+      human_readable_type_tesim: ['Collection'],
+      license_tesim: ['https://creativecommons.org/licenses/by/4.0/'],
+      medium_tesim: ['b&w negative']
     }
   end
 
@@ -75,12 +51,22 @@ RSpec.describe 'Search results page', type: :system, js: true do
 
     expect(page).to have_selector('.primary-metadata')
     expect(page).to have_selector('.secondary-metadata')
-    expect(page.find('div.secondary-metadata dd.blacklight-services_contact_ssm').text).to eq services_contact
-    expect(page.find('div.secondary-metadata dd.blacklight-local_identifier_ssm').text).to eq "Collection 686"
+    expect(page.find('div.secondary-metadata dd.blacklight-services_contact_ssm').text).to eq 'UCLA'
+    expect(page.find('div.secondary-metadata dd.blacklight-local_identifier_ssm').text).to eq 'Collection 686'
 
-    expect(page).to have_content "Bennett (Walter E.) Photographic Collection, 1937-1983 (bulk 1952-1982)"
+    expect(page).to have_content 'Bennett (Walter E.) Photographic Collection, 1937-1983 (bulk 1952-1982)'
     expect(page).to have_content 'Local identifier: Collection 686'
-    expect(page).to have_content "Description: #{description}"
+    expect(page).to have_content 'Description: description'
+  end
+
+  it 'displays headings' do
+    visit solr_document_path(id)
+    expect(page).to have_content 'Item Overview'
+    expect(page).to have_content 'Notes'
+    expect(page).to have_content 'Physical Description'
+    expect(page).to have_content 'Keywords'
+    expect(page).to have_content 'Find This Item'
+    expect(page).to have_content 'Access Condition'
   end
 
   context 'license' do
