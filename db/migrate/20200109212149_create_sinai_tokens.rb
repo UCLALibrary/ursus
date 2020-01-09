5dc769729957a2ed2823aa1a07665955fe101ca6
jendiamond <thejendiamond@gmail.com>
2020-01-09 15:39:37 -0800

    If table does not exist

--- db/migrate/20200109212149_create_sinai_tokens.rb
+++ db/migrate/20200109212149_create_sinai_tokens.rb
@@ -1,9 +1,10 @@
 class CreateSinaiTokens < ActiveRecord::Migration[5.1]
   def change
-    create_table :sinai_tokens do |t|
-      t.string :sinai_token
-
-      t.timestamps
+    if !table_exists?(:sinai_tokens)
+      create_table :sinai_tokens do |t|
+        t.string :sinai_token
+        t.timestamps
+      end
     end
   end
 end
