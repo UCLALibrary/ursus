145d63977f3cf707b8becaa8be12fb607122ad4d
Tinuola <tinuola@users.noreply.github.com>
2020-01-29 15:02:34 -0800

    Adding Andy's authn changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,93 +1,71 @@
 # frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
-  before_action :display_banner?
+  before_action :display_banner?, :sinai_authn_check
 
   def display_banner?
-    @path_check = ''
-    if cookie?
-      @display_option = "none"
+    if banner_cookie?
+      @beta_banner_display_option = "none"
     else
-      @display_option = "block"
+      @beta_banner_display_option = "block"
       set_banner_cookie
-    end
-    if Flipflop.sinai?
-      @current_site = "SinaiPOC"
-      sinai_authn_check
-    else
-      @current_site = "UCLA"
     end
   end
 
   def sinai_authn_check
-    # Checks to see if we are on the Login page and do nothing
-    if request.fullpath.include?(login_path)
-      @path_check = request.fullpath.include?(login_path)
+    return if ENV['SINAI_ID_BYPASS'] # skip auth in development
+    return if !Flipflop.sinai? || [login_path, version_path].include?(request.path) || sinai_authenticated?
+    if ucla_token?
+      set_auth_cookies
+      redirect_to cookies[:requested_path]
     else
-      # if the cookie named sinai_authenticated already exists
-      if sinai_cookie?
-        # do nothing
-        'sinai_cookie You have a valid cookie that is allowing you to browse the Sinai Digital Library.'
-      # elsif the token EMEL sent back is in the database
-      elsif ucla_token?
-        set_auth_cookie
-        set_iv_cookie
-        'ucla_token You have a valid cookie that is allowing you to browse the Sinai Digital Library.'
-      # else go to the button page
-      else
-        redirect_to "/login?callback=#{request.original_url}"
-      end
+      redirect_to redirect_target
     end
   end
 
-  def cookie?
-    @banner_cookie = cookies[:banner_display_option]
+  def banner_cookie?
+    cookies[:banner_display_option]
   end
 
   def set_banner_cookie
     cookies[:banner_display_option] = "banner_off"
   end
 
-  def sinai_cookie?
-    # Does user have the sinai cookie?
-    @sinai_cookie = cookies[:sinai_authenticated]
+  def sinai_authenticated?
+    cookies[:sinai_authenticated]
   end
 
   def ucla_token?
-    # Does user have the sinai token in the database?
-    params[:token].present? && SinaiToken.find_by(sinai_token: params[:token])
+    # does the request have a querystring containing a param named token and, if so, was it previously written to the database?
+    return true if params[:token].present? && SinaiToken.find_by(sinai_token: params[:token])
+    # does the request have a querystring containing the character "?token=" and, if so, extract the token
+    return false unless request.fullpath.include?("?token=")
+    returned_token_array = request.fullpath.split(/\?token=/)
+    returned_token = returned_token_array[1]
+    # is the extracted token in the database and did the user pass through the login page?
+    return true if SinaiToken.find_by(sinai_token: returned_token) && cookies[:requested_path]
+
+    false
   end
 
-  def set_session_cookie
-    session[:sinai_authenticated_test] = "authenticated"
-  end
-
-  def set_auth_cookie
-    @encryptd_str = create_encrypted_string
+  def set_auth_cookies
     cookies[:sinai_authenticated] = {
-      value: @cipher_text_unpacked,
-      expires: Time.zone.now + 90.days,
+      value: create_encrypted_string.unpack('H*')[0].upcase,
+      expires: Time.zone.now + 25.years,
       domain: ENV['DOMAIN']
     }
-  end
-
-  def set_iv_cookie
-    @iv_unpacked = @iv.unpack('H*')[0].upcase
     cookies[:initialization_vector] = {
-      value: @iv_unpacked,
-      expires: Time.zone.now + 90.days,
+      value: cipher_iv.unpack('H*')[0].upcase,
+      expires: Time.zone.now + 25.years,
       domain: ENV['DOMAIN']
     }
   end
 
   def create_encrypted_string
-    todays_date = Time.zone.today
-    cipher = OpenSSL::Cipher::AES256.new :CBC
     cipher.encrypt
-    @iv = cipher.random_iv
     cipher.key = ENV['CIPHER_KEY']
-    cipher.iv = @iv
-    @cipher_text_packed = cipher.update("Authenticated #{todays_date}") + cipher.final
-    @cipher_text_unpacked = @cipher_text_packed.unpack('H*')[0].upcase
+    cipher.iv = cipher_iv
+    cipher.update("Authenticated #{Time.zone.today}") + cipher.final
   end
 
   helper Openseadragon::OpenseadragonHelper
@@ -102,4 +80,18 @@ class ApplicationController < ActionController::Base
   def render_404
     render file: Rails.root.join('public', '404.html'), status: :not_found, layout: false
   end
+
+  private
+
+    def cipher
+      @cipher ||= OpenSSL::Cipher::AES256.new :CBC
+    end
+
+    def cipher_iv
+      @iv ||= cipher.random_iv
+    end
+
+    def redirect_target
+      "/login?callback=#{request.original_url}"
+    end
 end
