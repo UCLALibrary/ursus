e0d507297eabd87d54f95c91b989c02f1ef4c7f0
Parinita Mulak <pghorpade@library.ucla.edu>
2020-02-11 10:26:51 -0800

    Add Unit tests for Catalog Controller

--- spec/controllers/catalog_controller_spec.rb
+++ spec/controllers/catalog_controller_spec.rb
@@ -58,6 +58,53 @@ RSpec.describe CatalogController, type: :controller do
     end
   end
 
+  describe "show fields" do
+    let(:show_fields) do
+      controller
+        .blacklight_config
+        .show_fields.keys
+    end
+    let(:expected_show_fields) do
+      ["architect_tesim", "alternative_title_tesim", "ark_ssi",
+       "based_near_label_tesim", "binding_note_ssi", "caption_tesim",
+       "collation_ssi", "composer_tesim", "contents_note_tesim",
+       "contributor_tesim", "creator_tesim", "date_created_tesim",
+       "date_modified_tesim", "date_uploaded_tesim", "description_tesim",
+       "dimensions_tesim", "dlcs_collection_name_tesim", "extent_tesim",
+       "foliation_ssi", "format_tesim", "funding_note_tesim", "genre_tesim",
+       "geographic_coordinates_ssim", "human_readable_resource_type_tesim",
+       "human_readable_rights_statement_tesim", "human_readable_language_tesim",
+       "identifier_tesim", "illuminator_tesim", "illustrations_note_tesim",
+       "keyword_tesim", "latitude_tesim", "location_tesim", "local_identifier_ssm",
+       "longitude_tesim", "lyricist_tesim", "medium_tesim", "member_of_collections_ssim",
+       "named_subject_tesim", "oclc_tesim_ssi", "page_layout_ssim", "photographer_tesim",
+       "place_of_origin_tesim", "provenance_tesim", "publisher_tesim",
+       "repository_tesim", "rights_country_tesim", "rights_holder_tesim",
+       "scribe_tesim", "services_contact_ssm", "subject_tesim", "subject_topic_tesim",
+       "summary_tesim", "support_tesim", "title_tesim", "toc_tesim", "uniform_title_tesim"]
+    end
+
+    it 'has exactly expected show fields' do
+      expect(show_fields).to contain_exactly(*expected_show_fields)
+    end
+  end
+
+  describe "sort fields" do
+    let(:sort_fields) do
+      controller
+        .blacklight_config
+        .sort_fields.keys
+    end
+
+    let(:expected_sort_fields) do
+      ["score desc", "sort_title_ssort asc", "sort_title_ssort desc", "sort_year_isi desc", "sort_year_isi asc"]
+    end
+
+    it 'has exactly expected sort fields' do
+      expect(sort_fields).to contain_exactly(*expected_sort_fields)
+    end
+  end
+
   describe 'index fields' do
     let(:index_fields) do
       controller
@@ -84,5 +131,21 @@ RSpec.describe CatalogController, type: :controller do
     end
 
     it { expect(search_fields).to contain_exactly(*expected_search_fields) }
+  end
+
+  xdescribe "show action" do
+    before do
+      # allow(controller).to receive(:search_service).and_return(search_service)
+      # expect(search_service).to receive(:fetch).and_return([mock_response, mock_document])
+    end
+    # let(:doc_id) { '2007020969' }
+    # let(:mock_response) { instance_double(Blacklight::Solr::Response) }
+    # let(:mock_document) { instance_double(SolrDocument, export_formats: {}) }
+    # let(:search_service) { instance_double(Blacklight::SearchService) }
+
+    it "has collection count on Collection Item record page" do
+      # get :show, params: { id: doc_id }
+      # expect(assigns[:document]).not_to be_nil
+    end
   end
 end
