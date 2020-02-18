39963e3e5bcafad56cf2e7817c4631aa14900d03
jendiamond <thejendiamond@gmail.com>
2020-02-18 14:28:33 -0800

    Add application_controller unit tests
    Connected to [URS-627](https://jira.library.ucla.edu/browse/URS-627)
    
    - [x] Write/update tests for each method in the file.
    - [x] Use the unit test checklist
    - [x] Make sure each example runs fast (10s of milliseconds)
    
    ---
    
    Changes to be committed:
    + modified: `spec/controllers/application_controller_spec.rb`

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -13,7 +13,6 @@ RSpec.describe ApplicationController, type: :controller do
     allow(controller).to receive(:request).and_return(instance_double('ActionDispatch::Request', path: '/'))
     allow(controller).to receive(:set_auth_cookies)
     allow(controller).to receive(:sinai_authenticated?).and_return(false)
-    allow(controller).to receive(:ucla_token?).and_return(true)
     allow(controller).to receive(:version_path).and_return('/test_version')
     allow(ENV).to receive(:[]).and_call_original
     allow(ENV).to receive(:[]).with('SINAI_ID_BYPASS').and_return(nil)
@@ -50,6 +49,9 @@ RSpec.describe ApplicationController, type: :controller do
   end
 
   describe '#sinai_authn_check' do
+    before do
+      allow(controller).to receive(:ucla_token?).and_return(true)
+    end
     context 'if ucla token is set in URL parameters' do
       it 'calls set_auth_cookies' do
         controller.sinai_authn_check
@@ -74,8 +76,8 @@ RSpec.describe ApplicationController, type: :controller do
 
     context 'if ENV[\'SINAI_ID_BYPASS\'] is true' do
       before do
-        allow(ENV).to receive(:[]).and_call_original
-        allow(ENV).to receive(:[]).with('SINAI_ID_BYPASS').and_return(true)
+        allow(ENV).to receive(:[]).and_call_original # default
+        allow(ENV).to receive(:[]).with('SINAI_ID_BYPASS').and_return(true) # specific
       end
       it 'returns true sinai_authn_check' do
         expect(controller.sinai_authn_check).to be true
@@ -115,6 +117,100 @@ RSpec.describe ApplicationController, type: :controller do
       end
       it 'allows Rails to continue' do
         expect(controller.sinai_authn_check).to be true
+      end
+    end
+  end
+
+  describe "#ucla_token" do
+    let(:request_fullpath) { instance_double('request', path: '/some-path', fullpath: '/catalog/ark:/21198/z1b0085j?topic=edication?token=408626bb-87a0-46cb-a592-8ece576a745f') }
+    let(:foo) { instance_double("SinaiToken", sinai_token: "12345") }
+
+    before do
+      allow(request).to receive(:fullpath).and_return('/some-path?token=')
+    end
+
+    context "the querystring contains a param named: 'token' and it is in the database" do
+      before do
+        params = ActionController::Parameters.new(token: '12345')
+        allow(controller).to receive(:params).and_return(params)
+        allow(SinaiToken).to receive(:find_by).and_return(nil) # default
+        allow(SinaiToken).to receive(:find_by).with(sinai_token: params[:token]).and_return(foo) # specific case
+      end
+      it "returns true" do
+        expect(controller.ucla_token?).to be true
+      end
+    end
+
+    context "token is included in the query string but didn't get parsed because of emel bug" do
+      before do
+        allow(controller).to receive(:request).and_return(request_fullpath)
+        params = ActionController::Parameters.new(silly: '12345')
+        allow(controller).to receive(:params).and_return(params)
+        allow(SinaiToken).to receive(:find_by).with(sinai_token: params[:token]).and_return(foo)
+      end
+      context "token is found in the database" do
+        before do
+          allow(SinaiToken).to receive(:find_by).with(sinai_token: '408626bb-87a0-46cb-a592-8ece576a745f').and_return(foo)
+        end
+        it "returns true" do
+          expect(controller.ucla_token?).to be true
+        end
+      end
+      context "token is NOT found in the database" do
+        before do
+          allow(SinaiToken).to receive(:find_by).with(sinai_token: '408626bb-87a0-46cb-a592-8ece576a745f').and_return(nil)
+        end
+        it "returns false" do
+          expect(controller.ucla_token?).to be false
+        end
+      end
+    end
+
+    context "token is not included" do
+      let(:request_no_token) { instance_double('request', path: '/some-path', fullpath: '/catalog/ark:/21198/z1b0085j?topic=edication?lamb=408626bb-87a0-46cb-a592-8ece576a745f') }
+      before do
+        allow(controller).to receive(:request).and_return(request_no_token)
+        params = ActionController::Parameters.new(silly: '12345')
+        allow(controller).to receive(:params).and_return(params)
+        allow(SinaiToken).to receive(:find_by).with(sinai_token: params[:token]).and_return(foo)
+      end
+      it 'returns false' do
+        expect(controller.ucla_token?).to be false
+      end
+    end
+  end # describe ucla_token?
+
+  describe 'set_auth_cookies' do
+    context 'creates the sinai_authenticated cookie' do
+      let(:cookies) { {} }
+      before do
+        allow(controller).to receive(:cookies).and_return(cookies)
+        allow(controller).to receive(:create_encrypted_string).and_return('mock_encrypted_string')
+        allow(ENV).to receive(:[]).and_call_original
+        allow(ENV).to receive(:[]).with('CIPHER_KEY').and_return('Thispasswordisreallyhardtoguess?')
+        allow(ENV).to receive(:[]).with('DOMAIN').and_return('ucla.edu')
+        allow(controller).to receive(:set_auth_cookies).and_call_original
+        allow(controller).to receive(:cipher_iv).and_return('mock_cipher_iv')
+      end
+
+      it 'sets the sinai_authenticated cookie value' do
+        controller.set_auth_cookies
+        expect(cookies[:sinai_authenticated][:value]).to eq('mock_encrypted_string'.unpack('H*')[0].upcase)
+      end
+
+      it 'sets an expiration date for the sinai_authenticated cookie' do
+        controller.set_auth_cookies
+        expect(cookies[:sinai_authenticated][:expires]).to be_kind_of(Time)
+      end
+
+      it 'sets the initialization_vector cookie value' do
+        controller.set_auth_cookies
+        expect(cookies[:initialization_vector][:value]).to eq('mock_cipher_iv'.unpack('H*')[0].upcase)
+      end
+
+      it 'sets an expiration date for the initialization_vector cookie' do
+        controller.set_auth_cookies
+        expect(cookies[:initialization_vector][:expires]).to be_kind_of(Time)
       end
     end
   end
