8e0e8323c56a2e483c7229ae841562c568202e72
jendiamond <thejendiamond@gmail.com>
2020-02-20 16:10:29 -0800

    URS-672 Add terms of use page
    Connected to [URS-672](https://jira.library.ucla.edu/browse/URS-672)
    
    - [x] Please add a "Terms of Use" page to the Sinai UI.
    
    ---
    
    Changes to be committed:
    modified:   app/views/shared/_sinai_footer.html.erb
    new file:   app/views/static/terms-of-use.html.erb
    modified:   config/routes.rb
    modified:   app/assets/stylesheets/sinai/_static-pages.scss

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/copyrights_and_collections', to: 'static#copyright'
   get '/privacy_policy', to: 'static#privacy'
   get '/contact', to: 'static#contact'
+  get '/terms-of-use', to: 'static#terms-of-use'
   get '/migration_updates', to: 'static#migration_updates'
   get '/static', to: 'static#static_pages'
   get '/version', to: 'static#version'
