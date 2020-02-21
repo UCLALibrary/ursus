2a3471da56b7a2f7ace113447c4c8e606b431c2a
jendiamond <thejendiamond@gmail.com>
2020-02-20 16:41:59 -0800

    URS-666 Display features field for "Keywords" section
    Connected to [URS-666](https://jira.library.ucla.edu/browse/URS-666)
    
    - [x] Display the Features field on the item page in the Keywords section
    
    ---
    
    Changes to be committed:
    modified:   app/controllers/catalog_controller.rb
    modified:   app/services/search_field_service.rb
    modified:   spec/services/search_field_service_spec.rb
    modified:   spec/controllers/catalog_controller_spec.rb

--- spec/services/search_field_service_spec.rb
+++ spec/services/search_field_service_spec.rb
@@ -4,10 +4,9 @@ require 'rails_helper'
 
 RSpec.describe SearchFieldService do
   let(:search_field_service) { described_class.instance }
-
   it 'returns the correct list of search fields' do
     expect(search_field_service.search_fields).to eq(%w[ alternative_title_tesim architect_tesim binding_note_ssi caption_tesim collation_ssi composer_tesim contents_note_tesim
-                                                         contributor_tesim creator_tesim description_tesim foliation_ssi genre_tesim
+                                                         contributor_tesim creator_tesim description_tesim features_tesim foliation_ssi genre_tesim
                                                          identifier_tesim illuminator_tesim illustrations_note_tesim local_identifier_ssm location_tesim lyricist_tesim medium_tesim
                                                          named_subject_tesim normalized_date_tesim
                                                          page_layout_ssim photographer_tesim place_of_origin_tesim provenance_tesim
