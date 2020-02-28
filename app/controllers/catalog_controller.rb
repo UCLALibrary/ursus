d797407e3f804a8c2a11407d6039c2c5263c309f
jendiamond <thejendiamond@gmail.com>
2020-02-27 17:41:12 -0800

    remove author from facets

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -91,7 +91,6 @@ class CatalogController < ApplicationController
       config.add_facet_field 'script_sim', limit: 5, label: 'Script'
       config.add_facet_field 'features_sim', limit: 5, label: 'Features'
       config.add_facet_field 'support_sim', limit: 5, label: 'Support'
-      config.add_facet_field 'author_tesim', limit: 5, label: 'Author'
     else
       config.add_facet_field 'subject_sim', limit: 5, label: 'Subjects'
       # config.add_facet_field ::Solrizer.solr_name('resource_type', :facetable), limit: 5
