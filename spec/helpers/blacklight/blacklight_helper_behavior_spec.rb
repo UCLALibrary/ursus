ba5a5f47a8257a8a208055108eebdb4ff4ce0cb2
jendiamond <thejendiamond@gmail.com>
2020-04-27 16:39:32 -0700

    CAL-892 Review refactor tickets
    All the reffactor tickets have been reviewed by the team and are updated.
    Files that were not being used were removed.
    
    ---
    
    Changes to be committed:
    deleted:    app/controllers/concerns/.keep
    deleted:    app/helpers/application_helper.rb
    deleted:    app/helpers/banner_helper.rb
    deleted:    app/helpers/blacklight/icon_helper_behavior.rb
    modified:   app/helpers/ursus/catalog_helper.rb
    deleted:    spec/helpers/banner_helper_spec.rb
    new file:   spec/helpers/blacklight/blacklight_helper_behavior_spec.rb
    new file:   spec/helpers/blacklight/facet_label_button_behavior_spec.rb
    modified:   spec/helpers/blacklight_helper_spec.rb

---
+++ spec/helpers/blacklight/blacklight_helper_behavior_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe HomePageHelper, type: :helper do
+  describe '#link_to_featured_work' do
+    xit '' do
+    end
+  end
+end
