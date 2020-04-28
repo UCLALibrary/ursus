62c81eefe17053e7d88309ae41a1fe259df47f86
jendiamond <thejendiamond@gmail.com>
2020-04-28 16:56:07 -0700

    Add unit tests for contact_collection & find_collection presenters
    Connected to [URS-746](https://jira.library.ucla.edu/browse/URS-746) && [US-748](https://jira.library.ucla.edu/browse/URS-748)
    
    ---
    
    Changes to be committed:
    + new file:   spec/presenters/ursus/contact_collection_metadata_presenter_spec.rb
    + modified:   spec/presenters/ursus/find_collection_metadata_presenter_spec.rb

---
+++ spec/presenters/ursus/contact_collection_metadata_presenter_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe Ursus::ContactCollectionMetadataPresenter do
+  let(:solr_doc) do
+    { 'services_contact_ssm' => 'Rights Contact' }
+  end
+  let(:presenter_object) { described_class.new(document: solr_doc) }
+  let(:config) { YAML.safe_load(File.open(Rails.root.join('config', 'metadata/contact_collection_metadata.yml'))) }
+
+  context 'with a solr document containing Find Collection info metadata' do
+    describe 'config' do
+      it 'returns the Rights Contact Key' do
+        expect(config['services_contact_ssm'].to_s).to eq('Rights Contact')
+      end
+    end
+
+    describe "#contact_collection_terms" do
+      let(:all) { presenter_object.contact_collection_terms.keys.length }
+      let(:missing) { presenter_object_missing_items.contact_collection_terms.keys.length }
+
+      it "returns existing keys" do
+        expect(presenter_object.contact_collection_terms).to be_instance_of(Hash)
+        expect(all).to eq 1
+        expect(config.length).to eq all
+      end
+    end
+  end
+end
