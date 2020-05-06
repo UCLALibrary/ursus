fa1f87bf8c7553ee0467378bb1f199b65e36a3a1
parinita <pghorpade@library.ucla.edu>
2020-05-06 13:07:49 -0700

    APPS-445 Updates for the OAI package (#704)

--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 class SolrDocument
   include Blacklight::Solr::Document
+  include BlacklightOaiProvider::SolrDocument
+
   include Blacklight::Gallery::OpenseadragonSolrDocument
 
   # self.unique_key = 'id'
