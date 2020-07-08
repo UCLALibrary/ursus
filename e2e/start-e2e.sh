7312f75996da86fa25769e9c04510fadc6477bf8
Andrew Wallace <andrewwallace@library.ucla.edu>
2020-07-08 10:19:20 -0700

    set up initial percy.io visual tests

--- e2e/start-e2e.sh
+++ e2e/start-e2e.sh
@@ -1,3 +1,10 @@
 set -e
 
-npx cypress run
+# Only run percy in master (including PRs); otherwise just cypress
+# (Gotta ration our screenshots!)
+if [ "$TRAVIS_BRANCH" = "master" ] 
+then
+  npx percy exec -- npx cypress run
+else
+  npx cypress run
+fi
