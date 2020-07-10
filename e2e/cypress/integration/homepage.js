2eabc9c3641bd3b60a883b2c23bdddeebe439094
darrowcoucla <darrowco@library.ucla.edu>
2020-07-09 17:57:23 -0700

    Test created and working for the forst 3 or 4 actions

--- e2e/cypress/integration/homepage.js
+++ e2e/cypress/integration/homepage.js
@@ -1,6 +1,26 @@
+/// <reference types="Cypress" />
+
 describe('Ursus Homepage', () => {
   it('Visits the Homepage', () => {
-    cy.visit('/')
-    cy.percySnapshot()
-  })
-})
+    cy.visit('/');
+    cy.percySnapshot();
+  });
+  it('Visits the Homepageand clicks through to Los Angeles Daily News Negatives', () => {
+    cy.visit('https://digital.library.ucla.edu');
+    cy.contains('h3', 'Los Angeles Daily News Negatives').click();
+    cy.percySnapshot();
+  });
+  it('Visits the Homepageand clicks through to Walter E. Bennett Photographic Collection, 1937-1983', () => {
+    cy.visit('https://digital.library.ucla.edu');
+    cy.contains(
+      'h3',
+      'Walter E. Bennett Photographic Collection, 1937-1983'
+    ).click();
+    cy.percySnapshot();
+  });
+  it('Visits the Homepageand clicks through to Will Connell Papers', () => {
+    cy.visit('https://digital.library.ucla.edu');
+    cy.contains('h3', 'Will Connell Papers').click();
+    cy.percySnapshot();
+  });
+});
