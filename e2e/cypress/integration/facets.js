6c67e8d4aa127eb857cc6da55ad6a39e73616a12
Andrew Wallace <andrewwallace@library.ucla.edu>
2020-07-23 15:04:34 -0700

    edits to facet tests

--- e2e/cypress/integration/facets.js
+++ e2e/cypress/integration/facets.js
@@ -1,20 +1,25 @@
-describe('Ursus Homepage', () => {
-  it('Visits the Homepage, clicks on the Subject facet, clicks on more, clicks on A-Z Sort, clicks on next, clicks Academy Awards (Motion pictures) and verifies the page load', () => {
-    cy.visit('https://digital.library.ucla.edu');
+describe('Facets', () => {
+  it('Subject', () => {
+    cy.visit('/');
     cy.contains('a', 'Subject').click({ force: true });
+    cy.percySnapshot('Subject facet open');
+
     cy.contains('a', 'more').click({ force: true });
+    cy.contains('Landmarks');
+
     cy.contains('a', 'A-Z Sort').click({ force: true });
+    cy.contains('Abbots--Japan');
     cy.contains('a', 'Next').click({ force: true });
-    cy.get(
-      '.modal-footer > .facet-pagination > .modal-pagination > :nth-child(2) > .modal-pagination__page-link'
-    ).click({ force: true });
-    cy.get('a[href*="Academy"]').click({ force: true });
+    cy.percySnapshot('Subject facet modal');
+    cy.contains('Academy Awards (Motion pictures)').click({ force: true });
+
     cy.get('.filter-label-key').contains('Subject');
     cy.get('.filter-label-value').contains('Academy Awards (Motion pictures)');
-    cy.percySnapshot();
+    cy.percySnapshot('Subject facet selected');
   });
-  it('Visits the Homepage, opens on the Resource Type facet, clicks on Cartographic, opens on the Language facet, clicks on English and verifies page load', () => {
-    cy.visit('https://digital.library.ucla.edu');
+
+  it('Resource Type + Language', () => {
+    cy.visit('/');
     cy.contains('a', 'Resource Type').click();
     cy.contains('a', 'cartographic').click({ force: true });
     cy.get('[title="cartographic"]');
@@ -35,11 +40,12 @@ describe('Ursus Homepage', () => {
     ).contains('cartographic');
     cy.percySnapshot();
   });
-  it('Visits the Homepage, opens on the Genre facet, clicks on black-and-white photographs, opens on the Language facet, clicks on English and verifies page load', () => {
-    cy.visit('https://digital.library.ucla.edu');
+
+  it('Genre + Language', () => {
+    cy.visit('/');
     cy.contains('a', 'Genre').click();
-    cy.contains('a', 'black-and-white photographs').click({ force: true });
-    cy.get('[title="black-and-white photographs"]');
+    cy.contains('a', 'Black-and-white photographs').click({ force: true });
+    cy.get('[title="Black-and-white photographs"]');
     cy.contains('a', 'Language').click();
     cy.contains('a', 'English').click({ force: true });
     cy.get('[title="English"]');
@@ -48,26 +54,24 @@ describe('Ursus Homepage', () => {
     ).contains('Genre');
     cy.get(
       '.filter-genre_sim > .filter-group__label > .filter-label-value'
-    ).contains('black-and-white photographs');
+    ).contains('Black-and-white photographs');
     cy.get(
       '.filter-human_readable_language_sim > .filter-group__label > .filter-label-key'
     ).contains('Language');
     cy.get(
       '.filter-human_readable_language_sim > .filter-group__label > .filter-label-value'
     ).contains('English');
-    cy.percySnapshot();
   });
-  it('Visits the Homepage, opens on the Genre facet, clicks on any link, clicks on Start Over and verifies page load', () => {
-    cy.visit('https://digital.library.ucla.edu');
-    cy.contains('a', 'Genre').click();
-    cy.contains('a', 'black-and-white photographs').click({ force: true });
-    cy.get('[title="black-and-white photographs"]');
+
+  it('Start Over', () => {
+    cy.visit('/catalog?f[genre_sim][]=Black-and-white+photographs');
+    cy.get('[title="Black-and-white photographs"]');
     cy.contains('a', 'Start Over').click({ force: true });
-    cy.get('title').contains('UCLA Library Digital Collections Search Results');
-    cy.percySnapshot();
+    cy.get('.search-results-container').should('not.contain', 'You searched for');
   });
-  it('Visits the Homepage, opens on the Locations facet, clicks on Los Angeles (Calif.), opens on the Namse facet, clicks on Tournament of Roses and verifies page load', () => {
-    cy.visit('https://digital.library.ucla.edu');
+
+  it('Names', () => {
+    cy.visit('/');
     cy.contains('a', 'Names').click();
     cy.contains('a', 'Tournament of Roses').click({ force: true });
     cy.get('[title="Tournament of Roses"]');
@@ -88,22 +92,20 @@ describe('Ursus Homepage', () => {
     ).contains('Tournament of Roses');
     cy.percySnapshot();
   });
-  it('Visits the Homepage, opens on the Dates facet, enters a Starting Date, enters an ending date and verifies page load', () => {
-    cy.visit('https://digital.library.ucla.edu');
+
+  it('Dates', () => {
+    cy.visit('/');
     cy.contains('a', 'Date').click();
-    cy.get('#range_year_isim_begin').type(
-      '{end}{backspace}{backspace}{backspace}{backspace}1935'
-    );
-    cy.get('#range_year_isim_end').type(
-      '{end}{backspace}{backspace}{backspace}{backspace}1967'
-    );
+    cy.get('#range_year_isim_begin').clear().type('1935');
+    cy.get('#range_year_isim_end').clear().type('1967');
     cy.get('.submit').click();
     cy.get('.filter-label-key').contains('Date');
     cy.get('.filter-label-value').contains('1935 to 1967');
     cy.percySnapshot();
   });
-  it('Visits the Homepage, clicks on the Collection facet, clicks on more, clicks on next, clicks Ethiopic Manuscripts and verifies the page load', () => {
-    cy.visit('https://digital.library.ucla.edu');
+
+  it('Collection', () => {
+    cy.visit('/');
     cy.contains('a', 'Collection').click({ force: true });
     cy.get(
       '#facet-member_of_collections_ssim > .facet-values > .more_facets > a'
@@ -114,8 +116,9 @@ describe('Ursus Homepage', () => {
     cy.get('.filter-label-value').contains('Ethiopic Manuscripts');
     cy.percySnapshot();
   });
-  it('Visits the Homepage, clicks on the Genre facet, clicks on more, clicks on next, clicks Architectural drawings and verifies the page load', () => {
-    cy.visit('https://digital.library.ucla.edu');
+
+  it('Genre', () => {
+    cy.visit('/');
     cy.contains('a', 'Genre').click({ force: true });
     cy.get('#facet-genre_sim > .facet-values > .more_facets > a').click({
       force: true,
@@ -124,18 +127,6 @@ describe('Ursus Homepage', () => {
     cy.get('a[href*="Architectural+drawings"]').click({ force: true });
     cy.get('.filter-label-key').contains('Genre');
     cy.get('.filter-label-value').contains('Architectural drawings');
-    cy.percySnapshot();
-  });
-  it('Visits the Homepage, clicks on the Subject facet, clicks Landmarks and verifies the page load', () => {
-    cy.visit('https://digital.library.ucla.edu');
-    cy.contains('a', 'Subject').click({ force: true });
-    cy.get('#facet-subject_sim > .facet-values > .more_facets > a').click({
-      force: true,
-    });
-    ///cy.contains('a', 'A-Z Sort').click({ force: true });
-    cy.get('a[href*="Landmarks"]').click({ force: true });
-    cy.get('.filter-label-key').contains('Subject');
-    cy.get('.filter-label-value').contains('Landmarks');
     cy.percySnapshot();
   });
 });
