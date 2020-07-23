009c7c52826c04476905e76c162c942843603cb8
Darrow Cole <darrowcoucla_03@darrow.com>
2020-07-23 15:02:34 -0700

    Urs 893 implement cyress tests for collection pages (#856)
    Co-authored-by: darrowcoucla <darrowco@library.ucla.edu>
    Co-authored-by: Andrew Wallace <andrewwallace@library.ucla.edu>

---
+++ e2e/cypress/integration/collections.js
@@ -0,0 +1,38 @@
+describe('Collection pages', () => {
+  it('Los Angeles Daily News Negatives', () => {
+    cy.visit('/');
+    cy.contains('h3', 'Los Angeles Daily News Negatives').click();
+    cy.get('[class=banner__title--collection]').contains(
+      'Los Angeles Daily News Negatives'
+    );
+    cy.percySnapshot();
+  });
+
+  it('Walter E. Bennett Photographic Collection, 1937-1983', () => {
+    cy.visit('/');
+    cy.contains(
+      'h3',
+      'Walter E. Bennett Photographic Collection, 1937-1983'
+    ).click();
+    cy.get('[class=banner__title--collection]').contains(
+      'Bennett (Walter E.) Photographic Collection, 1937-1983 (bulk 1952-1982)'
+    );
+    cy.percySnapshot();
+  });
+
+  it('Will Connell Papers', () => {
+    cy.visit('/');
+    cy.contains('h3', 'Will Connell Papers').click();
+    cy.get('[class=banner__title--collection]').contains('Connell (Will) Papers');
+    cy.percySnapshot();
+  });
+
+  it('California Postcards - Collection Record', () => {
+    cy.visit('/');
+    cy.get(
+      '[href="/catalog/37sh8000zz-89112"] > .collection-grid__item-content > .collection-grid__item-link'
+    ).click();
+    cy.contains('.metadata-block__title', 'Collection Overview')
+    cy.percySnapshot();
+  });
+});
