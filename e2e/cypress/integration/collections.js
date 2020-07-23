81623a94684f72f94b189ba2f92a3ad0b978bcd9
Andrew Wallace <andrewwallace@library.ucla.edu>
2020-07-23 13:35:42 -0700

    edits

--- e2e/cypress/integration/collections.js
+++ e2e/cypress/integration/collections.js
@@ -1,32 +1,38 @@
-it('Visits the Homepage and clicks through to Los Angeles Daily News Negatives', () => {
-  cy.visit('https://digital.library.ucla.edu');
-  cy.contains('h3', 'Los Angeles Daily News Negatives').click();
-  cy.get('[class=banner__title--collection]').contains(
-    'Los Angeles Daily News Negatives'
-  );
-  cy.percySnapshot();
-});
-it('Visits the Homepage and clicks through to Walter E. Bennett Photographic Collection, 1937-1983', () => {
-  cy.visit('https://digital.library.ucla.edu');
-  cy.contains(
-    'h3',
-    'Walter E. Bennett Photographic Collection, 1937-1983'
-  ).click();
-  cy.get('[class=banner__title--collection]').contains(
-    'Bennett (Walter E.) Photographic Collection, 1937-1983 (bulk 1952-1982)'
-  );
-  cy.percySnapshot();
-});
-it('Visits the Homepage and clicks through to Will Connell Papers', () => {
-  cy.visit('https://digital.library.ucla.edu');
-  cy.contains('h3', 'Will Connell Papers').click();
-  cy.get('[class=banner__title--collection]').contains('Connell (Will) Papers');
-  cy.percySnapshot();
-});
-it('Visits the Homepage and clicks the More about this collection link of a collection', () => {
-  cy.visit('https://digital.library.ucla.edu');
-  cy.get(
-    '[href="/catalog/37sh8000zz-89112"] > .collection-grid__item-content > .collection-grid__item-link'
-  ).click();
-  cy.percySnapshot();
+describe('Collection pages', () => {
+  it('Los Angeles Daily News Negatives', () => {
+    cy.visit('/');
+    cy.contains('h3', 'Los Angeles Daily News Negatives').click();
+    cy.get('[class=banner__title--collection]').contains(
+      'Los Angeles Daily News Negatives'
+    );
+    cy.percySnapshot();
+  });
+
+  it('Walter E. Bennett Photographic Collection, 1937-1983', () => {
+    cy.visit('/');
+    cy.contains(
+      'h3',
+      'Walter E. Bennett Photographic Collection, 1937-1983'
+    ).click();
+    cy.get('[class=banner__title--collection]').contains(
+      'Bennett (Walter E.) Photographic Collection, 1937-1983 (bulk 1952-1982)'
+    );
+    cy.percySnapshot();
+  });
+
+  it('Will Connell Papers', () => {
+    cy.visit('/');
+    cy.contains('h3', 'Will Connell Papers').click();
+    cy.get('[class=banner__title--collection]').contains('Connell (Will) Papers');
+    cy.percySnapshot();
+  });
+
+  it('California Postcards - Collection Record', () => {
+    cy.visit('/');
+    cy.get(
+      '[href="/catalog/37sh8000zz-89112"] > .collection-grid__item-content > .collection-grid__item-link'
+    ).click();
+    cy.contains('.metadata-block__title', 'Collection Overview')
+    cy.percySnapshot();
+  });
 });
