940b6c57c018fa18a1664257e431f9f31abdfc18
darrowcoucla <darrowco@library.ucla.edu>
2020-08-24 18:05:44 -0700

    removed test url

--- e2e/cypress/integration/work-show.spec.js
+++ e2e/cypress/integration/work-show.spec.js
@@ -1,6 +1,6 @@
 describe('Work show pages', () => {
   it('Calendar of Feasts', () => {
-    cy.visit('https://digital.library.ucla.edu/catalog/qpvh6200zz-89112');
+    cy.visit('/catalog/qpvh6200zz-89112');
     cy.contains(
       'h1',
       'Manuscript No. 32: Calendar of Feasts 14th/15th Century'
@@ -8,10 +8,10 @@ describe('Work show pages', () => {
     cy.frameLoaded();
     cy.frameLoaded({
       url:
-        'https://digital.library.ucla.edu/uv/uv.html#?manifest=https%3A%2F%2Fiiif.library.ucla.edu%2Fark%253A%252F21198%252Fzz0026hvpq%2Fmanifest',
+        '/uv/uv.html#?manifest=https%3A%2F%2Fiiif.library.ucla.edu%2Fark%253A%252F21198%252Fzz0026hvpq%2Fmanifest',
     });
     cy.visit(
-      'https://digital.library.ucla.edu/uv/uv.html#?manifest=https%3A%2F%2Fiiif.library.ucla.edu%2Fark%253A%252F21198%252Fzz0026hvpq%2Fmanifest'
+      '/uv/uv.html#?manifest=https%3A%2F%2Fiiif.library.ucla.edu%2Fark%253A%252F21198%252Fzz0026hvpq%2Fmanifest'
     );
     cy.percySnapshot();
   });
