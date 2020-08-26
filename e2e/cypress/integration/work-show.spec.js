cc0ea02f229695a1f45a7c291b499541d4bc4f1e
darrowcoucla <darrowco@library.ucla.edu>
2020-08-26 11:55:19 -0700

    minor change to force test to run for PR

--- e2e/cypress/integration/work-show.spec.js
+++ e2e/cypress/integration/work-show.spec.js
@@ -10,7 +10,7 @@ describe('Work show pages', () => {
       url:
         '/uv/uv.html#?manifest=https%3A%2F%2Fiiif.library.ucla.edu%2Fark%253A%252F21198%252Fzz0026hvpq%2Fmanifest',
     });
-    cy.iframe().contains('span', 'Manuscript No.32: 001');
+    cy.iframe().contains('span', 'Manuscript No.32: 00');
     cy.visit(
       '/uv/uv.html#?manifest=https%3A%2F%2Fiiif.library.ucla.edu%2Fark%253A%252F21198%252Fzz0026hvpq%2Fmanifest'
     );
