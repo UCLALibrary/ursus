a4bcc6de6ba1c94b54c5c36971a9fa0e5317d663
Darrow Cole <darrowcoucla_03@darrow.com>
2020-08-26 13:31:38 -0700

    Urs 902 cypress tests for viewer windows (#882)
    * added iframe checks to cypress
    
    * removed test url
    
    * added iframe checks to cypress
    
    * added id of a UV element tot he test; added cypress.iframe to package.json
    
    * minor change to force test to run for PR
    
    * Update work-show.spec.js
    
    Co-authored-by: darrowcoucla <darrowco@library.ucla.edu>
    Co-authored-by: Andrew Wallace <andrewwallace@library.ucla.edu>

--- e2e/cypress/integration/work-show.spec.js
+++ e2e/cypress/integration/work-show.spec.js
@@ -1,19 +1,15 @@
 describe('Work show pages', () => {
   it('Calendar of Feasts', () => {
     cy.visit('/catalog/qpvh6200zz-89112');
-    cy.contains('h1', 'Manuscript No. 32: Calendar of Feasts 14th/15th Century');
-    cy.percySnapshot();
-  });
-
-  it('Ṣālot ba᾽eneta Mā᾽esaromu la- Agān᾽ent', () => {
-    cy.visit('/catalog/pnczp100zz-89112');
-    cy.contains('h1', 'Ms. 51 Ṣālot ba᾽eneta Mā᾽esaromu la- Agān᾽ent');
-    cy.percySnapshot();
-  });
-
-  it('John Binan and officers at the homicide crime scene of Louise Appier, Los Angeles, 1935', () => {
-    cy.visit('/catalog/c8f45200zz-89112');
-    cy.contains('h1', 'John Binan and officers at the homicide crime scene of Louise Appier, Los Angeles, 1935');
+    cy.contains(
+      'h1',
+      'Manuscript No. 32: Calendar of Feasts 14th/15th Century'
+    );
+    cy.frameLoaded({
+      url:
+        '/uv/uv.html#?manifest=https%3A%2F%2Fiiif.library.ucla.edu%2Fark%253A%252F21198%252Fzz0026hvpq%2Fmanifest',
+    });
+    cy.iframe().contains('span', 'Manuscript No.32: 00');
     cy.percySnapshot();
   });
 });
