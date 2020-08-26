6a5b998e0b1d2ca58383e03a9dd048deb6e9dee6
Andy Wallace <andrewwallace@library.ucla.edu>
2020-08-26 12:46:52 -0700

    Update work-show.spec.js

--- e2e/cypress/integration/work-show.spec.js
+++ e2e/cypress/integration/work-show.spec.js
@@ -5,15 +5,11 @@ describe('Work show pages', () => {
       'h1',
       'Manuscript No. 32: Calendar of Feasts 14th/15th Century'
     );
-    cy.frameLoaded();
     cy.frameLoaded({
       url:
         '/uv/uv.html#?manifest=https%3A%2F%2Fiiif.library.ucla.edu%2Fark%253A%252F21198%252Fzz0026hvpq%2Fmanifest',
     });
     cy.iframe().contains('span', 'Manuscript No.32: 00');
-    cy.visit(
-      '/uv/uv.html#?manifest=https%3A%2F%2Fiiif.library.ucla.edu%2Fark%253A%252F21198%252Fzz0026hvpq%2Fmanifest'
-    );
     cy.percySnapshot();
   });
 });
