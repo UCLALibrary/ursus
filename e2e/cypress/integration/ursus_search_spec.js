20f40203d71cb322323a1f5b5da526423519492b
Andrew Wallace <andrewwallace@library.ucla.edu>
2020-09-09 14:55:55 -0700

    fix problem with thumbnail links
    Backports a fix from blacklight 7.10
    
    see

--- e2e/cypress/integration/ursus_search_spec.js
+++ e2e/cypress/integration/ursus_search_spec.js
@@ -1,34 +1,41 @@
 /// <reference types="Cypress" />
-beforeEach(() => {
-  cy.visit('/');
-});
+
 describe('Search', () => {
   it('Search Blank', () => {
+    cy.visit('/');
     cy.get('[id=search]').click();
     cy.contains('span', 'You searched for:').should('not.exist');
     cy.percySnapshot();
   });
+
   it('Search Not Found', () => {
+    cy.visit('/');
     cy.get('[id=q]').type('unicorn');
     cy.get('[id=search]').click();
     cy.contains('h2', '0 Catalog Results').should('exist');
     cy.percySnapshot();
   });
+
   it('Search Title Found', () => {
+    cy.visit('/');
     cy.get('[id=q]').type('postcards');
     cy.get('select').select('Title').should('have.value', 'title_tesim');
     cy.get('[id=search]').click();
     cy.get('.search-count__heading').contains('Catalog Results');
     cy.percySnapshot();
   });
+
   it('Search Subject Found', () => {
+    cy.visit('/');
     cy.get('[id=q]').type('engineer');
     cy.get('select').select('Subject').should('have.value', 'subject_tesim');
     cy.get('[id=search]').click();
     cy.get('.search-count__heading').contains('Catalog Results');
     cy.percySnapshot();
   });
+
   it('Metadata Text', () => {
+    cy.visit('/');
     cy.contains('a', 'Resource Type').click();
     cy.contains('a', 'text').click({ force: true });
     cy.get('[title="text"]');
@@ -40,7 +47,9 @@ describe('Search', () => {
     cy.get('[title=text]').contains('text');
     cy.percySnapshot();
   });
+
   it('Metadata Still Image', () => {
+    cy.visit('/');
     cy.get('[id=q]').type('still image');
     cy.get('[id=search]').click();
     cy.get(
@@ -51,10 +60,23 @@ describe('Search', () => {
     cy.contains('span', 'still image');
     cy.percySnapshot();
   });
+
   it('Metadata Artistic Photo', () => {
     cy.visit('/catalog?utf8=%E2%9C%93&q=Artistic+Photo&search_field=all_fields');
     cy.get('a[href*="photographer_sim"]').eq(0).click();
     cy.contains('span', 'Photographer');
     cy.percySnapshot();
+  });
+
+  it('Title links to item page', () => {
+    cy.visit('/catalog?utf8=%E2%9C%93&q=&search_field=all_fields');
+    cy.get('h3.document__list-title').eq(0).find('a').click();
+    cy.get('.item-page__pagination-widgets').should('contain', '1 of')
+  });
+
+  it('Thumbnail links to item page', () => {
+    cy.visit('/catalog?utf8=%E2%9C%93&q=&search_field=all_fields');
+    cy.get('div.document__gallery-thumbnail').eq(0).find('img').click();
+    cy.get('.item-page__pagination-widgets').should('contain', '1 of')
   });
 });
