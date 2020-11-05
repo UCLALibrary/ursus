6e8c418ce167bc105f4d569df271f740810d759d
Ashton Prigge <ashton.prigge@gmail.com>
2020-11-05 14:16:46 -0800

    APPS-514: Added helper function for rendering html.

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -47,9 +47,9 @@ module BlacklightHelper
   end
 
   def render_other_versions_link
-    other_versions_link = @document[:other_versions_tesim]
+    other_versions_link = @document[:iiif_manifest_url_ssi]
     return unless other_versions_link
-    other_versions_link = "<dt class = 'metadata-block__label-key'>Other version</dt><dd class = 'metadata-block__label-value'><a href = '" + other_versions_link + "'>" + other_versions_link + "</a></dd>"
+    other_versions_link = "<dt class = 'metadata-block__label-key'>Other version(s)</dt><dd class = 'metadata-block__label-value'><a href = '" + other_versions_link + "'>" + other_versions_link + "</a></dd>"
     other_versions_link.html_safe
   end
 
--- spec/controllers/catalog_controller_spec.rb
+++ spec/controllers/catalog_controller_spec.rb
@@ -84,7 +84,7 @@ RSpec.describe CatalogController, type: :controller do
        "summary_tesim", "support_tesim", "title_tesim", "toc_tesim", "uniform_title_tesim",
        "hand_note_tesim", "writing_system_tesim", "commentator_tesim", "translator_tesim",
        "license_tesim", "normalized_date_sim", "subject_geographic_tesim", "subject_temporal_tesim",
-       "associated_name_tesim", "descriptive_title_tesim", "form_ssi", "references_tesim", "shelfmark_ssi","other_versions_tesim"]
+       "associated_name_tesim", "descriptive_title_tesim", "form_ssi", "references_tesim", "shelfmark_ssi", "other_versions_tesim"]
     end
 
     it 'has exactly expected show fields' do
