fd75cf1f8132b742265a5ada84027357e362220e
Ashton Prigge <ashton.prigge@gmail.com>
2020-11-12 09:20:22 -0800

    APPS-745: Add specific s3 url for each environment.

--- app/services/iiif_service.rb
+++ app/services/iiif_service.rb
@@ -6,9 +6,9 @@ class IiifService
       "#{request&.base_url}/mirador.html#?manifest=#{CGI.escape(iiif_manifest_url(document))}"
     else
       if request.query_parameters.include?('cv')
-        "https://t-w-dl-viewer01.library.ucla.edu//uv.html#?cv=#{request.query_parameters['cv']}&manifest=#{CGI.escape(iiif_manifest_url(document))}"
+        "https://t-w-dl-viewer01.library.ucla.edu/uv.html#?cv=#{request.query_parameters['cv']}&manifest=#{CGI.escape(iiif_manifest_url(document))}"
       else
-          "https://t-w-dl-viewer01.library.ucla.edu//uv.html#?manifest=#{CGI.escape(iiif_manifest_url(document))}"
+        "https://t-w-dl-viewer01.library.ucla.edu/uv.html#?manifest=#{CGI.escape(iiif_manifest_url(document))}"
       end
     end
   end
