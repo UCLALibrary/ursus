e08fcbc2e83645f7b2ec0d7a9d5d0318d0be30b8
Ashton Prigge <34147754+aprigge@users.noreply.github.com>
2020-11-17 13:13:00 -0800

    Apps 745/Switch to separate static UV - URS (#920)
    * APPS-745: Replace base_url with S3 Bucket url for UV.
    
    * APPS-745: Update Cypress test for cross origin iframes.
    
    * APPS-745: Update Cypress test for cross origin iframes.
    
    * APPS-745: Add specific s3 url for each environment.
    
    * APPS-745: Add specific s3 url for each environment.
    
    * APPS-745: Add specific s3 url for each environment.
    
    * APPS-745: Add specific s3 url for each environment.
    
    * APPS-745: Add specific s3 url for each environment.
    
    * APPS-745: Add specific s3 url for each environment.
    
    * APPS-745: Update iiif_service.rb to match on RAILS_HOST ENV variable.
    
    * APPS-745: Update iiif_service.rb to match on RAILS_HOST ENV variable.
    
    * APPS-745: Add check for web rails_env for ci.
    
    * APPS-745: Add check for web rails_env for ci.
    
    * APPS-745: Change https to http to avoid mixed content errors.
    
    * APPS-745: sub http for https in manifest url.
    
    * APPS-745: sub http for https in manifest url.

--- app/services/iiif_service.rb
+++ app/services/iiif_service.rb
@@ -34,7 +34,7 @@ class IiifService
 
   def iiif_manifest_url(document)
     if Flipflop.use_manifest_store? && document[:iiif_manifest_url_ssi]
-      document[:iiif_manifest_url_ssi]
+      document[:iiif_manifest_url_ssi].sub('http:', 'https:')
     else
       "#{Rails.application.config.iiif_url}/#{document.id}/manifest"
     end
