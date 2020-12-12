31817abf2c3580d638249a360bc887b1dbf87bc1
Tinuola <tinuola@users.noreply.github.com>
2020-12-11 18:42:39 -0800

    Add secondary navigation for static pages Misc style and layout updates: - Updated naming of Sinai colors for easier future updates - Made entire site logo area (icon and text titles) clickable - Updated color of page elements on index, item and home pages - Refactored CSS
    modified:   app/assets/stylesheets/legacy/ursus/_colors.scss
    modified:   app/assets/stylesheets/theme_sinai/components/_si-facets-modal.scss
    modified:   app/assets/stylesheets/theme_sinai/components/_si-facets.scss
    modified:   app/assets/stylesheets/theme_sinai/components/_si-gallery-view.scss
    modified:   app/assets/stylesheets/theme_sinai/components/_si-list-view.scss
    modified:   app/assets/stylesheets/theme_sinai/components/_si-metadata-block.scss
    modified:   app/assets/stylesheets/theme_sinai/components/_si-pagination-block.scss
    modified:   app/assets/stylesheets/theme_sinai/components/_si-search-count.scss
    modified:   app/assets/stylesheets/theme_sinai/components/_si-search-filter.scss
    modified:   app/assets/stylesheets/theme_sinai/components/_si-static-page-nav.scss
    modified:   app/assets/stylesheets/theme_sinai/elements/_si-buttons.scss
    modified:   app/assets/stylesheets/theme_sinai/elements/_si-colors.scss
    modified:   app/assets/stylesheets/theme_sinai/elements/_si-dividers.scss
    modified:   app/assets/stylesheets/theme_sinai/elements/_si-links.scss

--- e2e/cypress/integration/sinai_login_spec.js
+++ e2e/cypress/integration/sinai_login_spec.js
@@ -1,7 +1,7 @@
 describe('Sinai Login', () => {
   it('Login page', () => {
     cy.visit(Cypress.env('SINAI_BASE_URL') + '/login');
-    cy.get('.site-navbar__logo--sinai').should('be.visible');
+    cy.get('.site-navbar__logo-block--sinai').should('be.visible');
     cy.get('.site-navbar__logo-subtitle--sinai').should('be.visible');
     cy.get('.site-searchbar--sinai').should('not.exist');
     cy.get('.si-auth-page__title').should('be.visible');
