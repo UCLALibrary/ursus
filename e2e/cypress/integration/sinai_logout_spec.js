b3f7062b35d5d17d9f3fa0eec7500463729b7b5e
pghorpade <pghorpade@library.ucla.edu>
2020-12-16 18:53:44 -0800

    Remove logout cypress test

--- e2e/cypress/integration/sinai_logout_spec.js
+++
@@ -1,8 +0,0 @@
-describe('Sinai Logout', () => {
-  it('Logout', () => {
-    cy.clearCookie('sinai_authenticated');
-    cy.setCookie('sinai_authenticated', 'true');
-    cy.visit(Cypress.env('SINAI_BASE_URL'));
-    cy.contains('a', 'Logout');
-  });
-});
