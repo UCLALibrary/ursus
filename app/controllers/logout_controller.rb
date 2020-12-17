337856118102b2169546d82a9a65b649b8721b55
pghorpade <pghorpade@library.ucla.edu>
2020-12-16 16:09:09 -0800

    Reset session

--- app/controllers/logout_controller.rb
+++ app/controllers/logout_controller.rb
@@ -10,6 +10,7 @@ class LogoutController < ApplicationController
       cookies.delete :initialization_vector, domain: ENV['DOMAIN']
       @requested_path = cookies[:request_original_url]
       cookies.delete :request_original_url
+      reset_session
       redirect_to @requested_path
     end
   end
