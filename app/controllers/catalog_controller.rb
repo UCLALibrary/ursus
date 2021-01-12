9cc295db6237f16ba034a7e89ba40890fa5cf97f
Ashton Prigge <ashton.prigge@gmail.com>
2021-01-12 15:55:14 -0800

    APPS-559: Add sort: index to field facets.

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -106,16 +106,16 @@ class CatalogController < ApplicationController
 
     # SINAI
     if Flipflop.sinai?
-      config.add_facet_field 'genre_sim', limit: 7
-      config.add_facet_field 'place_of_origin_sim', limit: 7, label: 'Place of origin'
+      config.add_facet_field 'genre_sim', sort: 'index', limit: 7
+      config.add_facet_field 'place_of_origin_sim', sort: 'index', limit: 7, label: 'Place of origin'
       config.add_facet_field 'year_isim', limit: 7, range: true
-      config.add_facet_field 'human_readable_language_sim', limit: 7
-      config.add_facet_field 'writing_system_sim', limit: 7, label: 'Writing system'
-      config.add_facet_field 'script_sim', limit: 7, label: 'Script'
-      config.add_facet_field 'features_sim', limit: 7, label: 'Features'
-      config.add_facet_field 'support_sim', limit: 7, label: 'Support'
-      config.add_facet_field 'form_sim', limit: 7, label: 'Form'
-      config.add_facet_field 'names_sim', limit: 7, label: 'Names'
+      config.add_facet_field 'human_readable_language_sim',sort: 'index', limit: 7
+      config.add_facet_field 'writing_system_sim',sort: 'index', limit: 7, label: 'Writing system'
+      config.add_facet_field 'script_sim',sort: 'index', limit: 7, label: 'Script'
+      config.add_facet_field 'features_sim',sort: 'index', limit: 7, label: 'Features'
+      config.add_facet_field 'support_sim',sort: 'index', limit: 7, label: 'Support'
+      config.add_facet_field 'form_sim',sort: 'index', limit: 7, label: 'Form'
+      config.add_facet_field 'names_sim',sort: 'index', limit: 7, label: 'Names'
 
     # URSUS
     else
