eab18f65a300df3c570d8d4c2c060e667965ce51
jendiamond <thejendiamond@gmail.com>
2021-01-26 16:46:03 -0800

    Fix linting errors

--- app/processors/custom_join.rb
+++ app/processors/custom_join.rb
@@ -4,12 +4,7 @@ class CustomJoin < Blacklight::Rendering::AbstractStep
   include ActionView::Helpers::TextHelper
 
   def render
-    if Flipflop.sinai?
-        joiner = config.join_with || ' | '.html_safe
-        next_step(safe_join(values, joiner))
-    else
-      joiner = config.join_with || '<br>'.html_safe
-      next_step(safe_join(values, joiner))
-    end
+    joiner = Flipflop.sinai? ? (config.join_with || ' | '.html_safe) : (config.join_with || '<br>'.html_safe)
+    next_step(safe_join(values, joiner))
   end
 end
