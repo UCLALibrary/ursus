bb666e84e22986a82be7deac28160d280b45015d
jendiamond <thejendiamond@gmail.com>
2021-02-10 10:27:08 -0800

    Fix Cypress test

--- e2e/cypress/integration/sinai_search_spec.js
+++ e2e/cypress/integration/sinai_search_spec.js
@@ -23,14 +23,13 @@ describe('Sinai Search', () => {
     cy.get('[id=q]').type('manuscript');
     cy.get('[id=search]').click();
     cy.get('.search-count__heading').contains('Catalog Results');
-    cy.get('#documents > article.document.document__list-item.blacklight-work.document-position-0 > div > span > header > a:nth-child(2)').click();
-    // cy.get('.document-position-1 > .document__list-item-wrapper > .document__list-title > a').click();
+    cy.get('.document__gallery-thumbnail--sinai-generic').click({ force: true });
     cy.contains('h4','Item Overview');
   });
 
   it('Search Shelfmark Found', () => {
     cy.get('[id=q]').type('sinai syriac 100');
-    cy.get('select').select('Shelfmark').should('have.value', 'shelfmark_tsi');
+    cy.get('select').select('Shelfmark').should('have.value', 'shelfmark_ssi');
     cy.get('[id=search]').click();
     cy.get('.search-count__heading').contains('Catalog Results');
     cy.percySnapshot();
