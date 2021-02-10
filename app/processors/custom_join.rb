a22072101d18b82adefcc09ca36a9dff3b78ca31
jendiamond <thejendiamond@gmail.com>
2021-02-09 16:11:38 -0800

    Strip down styles

--- app/processors/custom_join.rb
+++ app/processors/custom_join.rb
@@ -4,7 +4,7 @@ class CustomJoin < Blacklight::Rendering::AbstractStep
   include ActionView::Helpers::TextHelper
 
   def render
-    joiner = Flipflop.sinai? ? (config.join_with || ' | '.html_safe) : (config.join_with || '<br>'.html_safe)
+    joiner = Flipflop.sinai? ? (config.join_with || '&nbsp;|&nbsp;'.html_safe) : (config.join_with || '<br>'.html_safe)
     next_step(safe_join(values, joiner))
   end
 end
