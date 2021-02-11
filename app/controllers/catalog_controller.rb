bf9367ed21a9c241969f31ef50d9a4e8c14cfab0
JenDiamond <thejendiamond@gmail.com>
2021-02-11 15:02:45 -0800

    Remove all of the `limit: 7` for the Sinai facets so scrolling feature will work

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -106,16 +106,16 @@ class CatalogController < ApplicationController
 
     # SINAI
     if Flipflop.sinai?
-      config.add_facet_field 'genre_sim', sort: 'index', limit: 7
-      config.add_facet_field 'place_of_origin_sim', sort: 'index', limit: 7, label: 'Place of origin'
-      config.add_facet_field 'year_isim', limit: 7, range: true
-      config.add_facet_field 'human_readable_language_sim', sort: 'index', limit: 7
-      config.add_facet_field 'writing_system_sim', sort: 'index', limit: 7, label: 'Writing system'
-      config.add_facet_field 'script_sim', sort: 'index', limit: 7, label: 'Script'
-      config.add_facet_field 'features_sim', sort: 'index', limit: 7, label: 'Features'
-      config.add_facet_field 'support_sim', sort: 'index', limit: 7, label: 'Support'
-      config.add_facet_field 'form_sim', sort: 'index', limit: 7, label: 'Form'
-      config.add_facet_field 'names_sim', sort: 'index', limit: 7, label: 'Names'
+      config.add_facet_field 'genre_sim', sort: 'index'
+      config.add_facet_field 'place_of_origin_sim', sort: 'index', label: 'Place of origin'
+      config.add_facet_field 'year_isim', range: true
+      config.add_facet_field 'human_readable_language_sim', sort: 'index'
+      config.add_facet_field 'writing_system_sim', sort: 'index', label: 'Writing system'
+      config.add_facet_field 'script_sim', sort: 'index', label: 'Script'
+      config.add_facet_field 'features_sim', sort: 'index', label: 'Features'
+      config.add_facet_field 'support_sim', sort: 'index', label: 'Support'
+      config.add_facet_field 'form_sim', sort: 'index', label: 'Form'
+      config.add_facet_field 'names_sim', sort: 'index', label: 'Names'
 
     # URSUS
     else
