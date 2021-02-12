ed57fff1ff4db49e0d0d767b8e66c34fef4d1994
darrowcoucla <darrowco@library.ucla.edu>
2021-02-11 18:30:07 -0800

    yaml and presenters for Sinai

--- app/presenters/sinai/codicology_metadata_presenter.rb
+++ app/presenters/sinai/codicology_metadata_presenter.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 module Sinai
-  class ContentMetadataPresenter
+  class CodicologyMetadataPresenter
     attr_reader :document
 
     def initialize(document:)
       @document = document
-      @config = YAML.safe_load(File.open(Rails.root.join('config', 'metadata-sinai/content_metadata.yml')))
+      @config = YAML.safe_load(File.open(Rails.root.join('config', 'metadata-sinai/codicology_metadata.yml')))
     end
 
     def overview_terms
