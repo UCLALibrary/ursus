20bb0d700149210aadc26a7116196375a27794b3
Ashton Prigge <ashton.prigge@gmail.com>
2021-04-28 09:19:06 -0700

    Increase cypress timeouts.

--- e2e/cypress/integration/ursus_collections_spec.js
+++ e2e/cypress/integration/ursus_collections_spec.js
@@ -1,6 +1,6 @@
 describe('Collection pages', () => {
   it('Los Angeles Daily News Negatives', () => {
-    cy.visit('/', { responseTimeout: 31000 });
+    cy.visit('/', { responseTimeout: 100000 });
     cy.contains('h3', 'Los Angeles Daily News Negatives').click();
     cy.get('[class=banner__title--collection]', { timeout: 100000 }).contains(
       'Los Angeles Daily News Negatives'
@@ -9,7 +9,7 @@ describe('Collection pages', () => {
   });
 
   it('Walter E. Bennett Photographic Collection, 1937-1983', () => {
-    cy.visit('/', { responseTimeout: 31000 });
+    cy.visit('/', { responseTimeout: 100000 });
     cy.contains(
       'h3',
       'Walter E. Bennett Photographic Collection, 1937-1983'
@@ -21,14 +21,14 @@ describe('Collection pages', () => {
   });
 
   it('Will Connell Papers', () => {
-    cy.visit('/', { responseTimeout: 31000 });
+    cy.visit('/', { responseTimeout: 100000 });
     cy.contains('h3', 'Will Connell Papers').click();
     cy.get('[class=banner__title--collection]', { timeout: 100000 }).contains('Connell (Will) Papers');
     cy.percySnapshot();
   });
 
   it('California Postcards - Collection Record', () => {
-    cy.visit('/', { responseTimeout: 31000 });
+    cy.visit('/', { responseTimeout: 100000 });
     cy.get(
       '[href="/catalog/ark:/21198/zz0008hs73"] > .collection-grid__item-content > .collection-grid__item-link'
     ).click();
