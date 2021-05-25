87465765924d7476fcd607d6855c18603e2f1595
Andrew Wallace <andrewwallace@library.ucla.edu>
2021-05-25 14:15:59 -0700

    fix bug with cannonical URL redirect
    CatalogController#cannonical_url_redirect recognizes potential old-style, reversed-ark URLs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
-# rubocop:disable Metrics/ClassLength:
 class ApplicationController < ActionController::Base
-  before_action :display_banner?, :sinai_authn_check, :add_legacy_views, :cors_preflight_check
+  before_action :display_banner?, :add_legacy_views, :cors_preflight_check
   after_action :cors_set_access_control_headers
   helper_method :solr_document_path, :solr_document_url
 
@@ -38,65 +37,12 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def sinai_authn_check
-    return true if !Flipflop.sinai? || [login_path, version_path].include?(request.path) || sinai_authenticated?
-    if ENV['SINAI_ID_BYPASS'] # skip auth in development
-      cookies[:sinai_authenticated] = 'true'
-      return true
-    end
-    check_document_paths
-    return unless ucla_token?
-    set_auth_cookies
-    redirect_to cookies[:requested_path]
-  end
-
-  def check_document_paths
-    redirect_to redirect_target if params[:id] && [solr_document_path(params[:id])].include?(request.path) # check if someone bookmarked the show page
-  end
-
   def banner_cookie?
     cookies[:banner_display_option]
   end
 
   def set_banner_cookie
     cookies[:banner_display_option] = "banner_off"
-  end
-
-  def sinai_authenticated?
-    cookies[:sinai_authenticated]
-  end
-
-  def ucla_token?
-    # does the request have a querystring containing a param named token and, if so, was it previously written to the database?
-    return true if params[:token].present? && SinaiToken.find_by(sinai_token: params[:token])
-    # does the request have a querystring containing the character "?token=" and, if so, extract the token
-    return false unless request.fullpath.include?("?token=")
-    returned_token_array = request.fullpath.split(/\?token=/)
-    returned_token = returned_token_array[1]
-    # is the extracted token in the database and did the user pass through the login page?
-    return true if SinaiToken.find_by(sinai_token: returned_token) && cookies[:requested_path]
-
-    false
-  end
-
-  def set_auth_cookies
-    cookies[:sinai_authenticated] = {
-      value: create_encrypted_string.unpack('H*')[0].upcase,
-      expires: Time.zone.now + 25.years,
-      domain: ENV['DOMAIN']
-    }
-    cookies[:initialization_vector] = {
-      value: cipher_iv.unpack('H*')[0].upcase,
-      expires: Time.zone.now + 25.years,
-      domain: ENV['DOMAIN']
-    }
-  end
-
-  def create_encrypted_string
-    cipher.encrypt
-    cipher.key = ENV['CIPHER_KEY']
-    cipher.iv = cipher_iv
-    cipher.update("Authenticated #{Time.zone.today}") + cipher.final
   end
 
   helper Openseadragon::OpenseadragonHelper
@@ -129,19 +75,4 @@ class ApplicationController < ActionController::Base
   def solr_document_url(*args)
     request.scheme + '://' + request.host_with_port + solr_document_path(*args)
   end
-
-  private
-
-    def cipher
-      @cipher ||= OpenSSL::Cipher::AES256.new :CBC
-    end
-
-    def cipher_iv
-      @iv ||= cipher.random_iv
-    end
-
-    def redirect_target
-      cookies[:request_original_url] = request.original_url
-      "/login"
-    end
 end
