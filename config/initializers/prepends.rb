da285fc718b84e8247af40f7fd9b50e2150ae9a6
Andrew Wallace <andrewwallace@library.ucla.edu>
2021-05-28 08:25:15 -0700

    Show default thumbnails by resource type APPS-899
    When `thumbnail_url_ss` is unpopulated, insert a default based on `resource_type_ssm`. Currently only populates for the a/v types specified in https://github.com/UCLALibrary/californica/blob/main/config/authorities/resource_types.yml, anything else is left `nil`.
    
    Also:
    - Use Ursus::ThumbnailPresenter by subclassing Blacklight::ThumbnailPresenter, rather than prepending a module into it (ran into some really weird behavior trying to use byebug w/ spring in the prepended module).
    - deleted some sinai code I came across

--- config/initializers/prepends.rb
+++ config/initializers/prepends.rb
@@ -1,4 +1,3 @@
 # frozen_string_literal: true
 CatalogHelper.send(:prepend, Blacklight::FacetLabelButtonBehavior)
-Blacklight::ThumbnailPresenter.send(:prepend, Ursus::ThumbnailPresenter)
 Blacklight::BlacklightHelperBehavior.send(:prepend, Blacklight::UrsusLayoutHelperBehavior)
