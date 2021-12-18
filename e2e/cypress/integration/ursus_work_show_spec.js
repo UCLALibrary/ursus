620f3195013df9b90e3debf5a76e329aa4b2c074
pghorpade <pghorpade@library.ucla.edu>
2021-12-17 16:35:56 -0800

    fix nested iframes issue

--- e2e/cypress/integration/ursus_work_show_spec.js
+++ e2e/cypress/integration/ursus_work_show_spec.js
@@ -10,9 +10,9 @@ describe('Work show pages', () => {
       url:
         'https://p-w-dl-viewer01.library.ucla.edu/#?manifest=https%3A%2F%2Fiiif.library.ucla.edu%2Fark%253A%252F21198%252Fzz0009q5nq%2Fmanifest',
     });
-   // TODO find the title of the manuscript insided nested iframes
-    cy.frameLoaded('iframe#media-viewer-iframe').iframeCustom().find('iframe#universalviewer-iframe').iframeCustom();
-    
+    cy.iframe().within(() => {
+      cy.iframe().contains('div', 'Ms. 35 Arganon')
+    });
     cy.percySnapshot();
   });
 });
