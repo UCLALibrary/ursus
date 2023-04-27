7586f572e56fcccec104935f220f8f74671738c8
pghorpade <pghorpade@library.ucla.edu>
2023-04-27 13:27:24 -0700

    fix: rubcop errors

--- lib/ucla/oai/collection_solr_set.rb
+++ lib/ucla/oai/collection_solr_set.rb
@@ -7,7 +7,6 @@ module Ucla
 
       # Build a set object with, at minimum, a set spec string
       def initialize(spec)
-        
         @label, @value = spec.split(':', 2)
         config = self.class.field_config_for(label)
         @solr_field = config[:solr_field] || label
@@ -15,7 +14,7 @@ module Ucla
         raise OAI::ArgumentException if @solr_field.blank?
       end
 
-       # Returns array of sets for a solr document, or empty array if none are available.
+      # Returns array of sets for a solr document, or empty array if none are available.
       def sets_for(record)
         Array.wrap(@fields).map do |field|
           record.fetch(field[:solr_field], []).map do |value|
