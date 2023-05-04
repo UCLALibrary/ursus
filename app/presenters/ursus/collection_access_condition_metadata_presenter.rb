76008c61f627a911e8083bb47b742217c67b21b5
jendiamond <thejendiamond@gmail.com>
2023-05-03 23:19:29 -0700

    aAdd collection presenter

---
+++ app/presenters/ursus/collection_access_condition_metadata_presenter.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+module Ursus
+    class CollectionAccessConditionMetadataPresenter
+      attr_reader :document
+  
+      def initialize(document:)
+        @document = document
+        @config = YAML.safe_load(File.open(Rails.root.join('config', 'metadata/collections_access_condition_metadata.yml')))
+      end
+  
+      def collection_access_condition_terms
+        @document.slice(*@config.keys)
+      end
+    end
+  end
+  
