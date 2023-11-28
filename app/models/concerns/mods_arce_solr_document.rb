fe9727f53e9ee1cc5429c28ad2e47b3fc84a84d0
jendiamond <thejendiamond@gmail.com>
2023-11-28 11:22:31 -0800

    update mods_arce_solr_document with related_records

--- app/models/concerns/mods_arce_solr_document.rb
+++ app/models/concerns/mods_arce_solr_document.rb
@@ -81,6 +81,13 @@ module ModsArceSolrDocument
             end
           end
         end
+        if self[:related_record_ssm]
+          xml['mods'].relatedItem(otherType: "relatedRecord") do
+            xml['mods'].titleInfo do
+              self[:related_record_ssm]&.each { |related_record| xml['mods'].identifier({ type: 'arc' }, related_record.to_s) }
+            end
+          end
+        end
         if self[:related_to_ssm]
           xml['mods'].relatedItem(otherType: "relatedTo", otherTypeAuth: "Bibframe") do
             xml['mods'].titleInfo do
