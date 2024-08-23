0457e2a010b6acefd9f8c1d8dd727b38057c252e
kethlinmil <tatiana.smirnova@nopio.com>
2024-08-23 19:51:51 +0200

    feat: APPS-2680 html tags and "read more" button for the search results

--- app/helpers/ursus/catalog_helper.rb
+++ app/helpers/ursus/catalog_helper.rb
@@ -29,8 +29,7 @@ module Ursus
         description = args[:value].first
         button = "<span class='view-more' href>Read More <div class='down-arrow'>&raquo;</div></span></br>"
         truncated_output << "<div class='description'>#{description}</div>#{button}</br>"
-        # return truncated_output.html_safe
-        return description
+        truncated_output.html_safe
       end
     end
   end
