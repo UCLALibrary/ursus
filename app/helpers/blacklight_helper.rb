beadb3e09fa4c6670ce1df979b2ecec201243fc9
Tatiana Smirnova <tatiana.smirnova@nopio.com>
2025-09-13 01:19:10 +0200

    [NOP-115] License Displaying Incorrect CC License (#1210)
    * feat: NOP-115 CC License Displaying
    
    * rspec
    
    * Public Domain mark 1.0
    
    * Public Domain mark 1.0

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -7,24 +7,34 @@ module BlacklightHelper
   def render_license(_args = {})
     return '' unless @document
     return '' unless @document[:license_tesim]
+
     license = @document[:license_tesim].first
+
     if license.match?(/creativecommons.org/)
-      data = license_markup
+      license_data = Ursus::LicenseMetadataPresenter.new(document: @document).data
+      return license unless license_data
+
+      data = license_markup(license_data)
       data.html_safe
     else
       license
     end
   end
 
-  def license_markup
-    %( <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">
-      <img alt="Creative Commons License" style="border-width:0"
-      src="https://i.creativecommons.org/l/by/4.0/88x31.png" />
-      </a><br />This work is licensed under a
-      <a rel="license"
-      href="http://creativecommons.org/licenses/by/4.0/">
-      Creative Commons Attribution 4.0 International License
-      </a>. )
+  def license_markup(license_data)
+    html = ""
+
+    if license_data['image'].present?
+      html += %(
+        <a rel="license" href="#{license_data['id']}">
+          <img alt="#{license_data['term']}" style="border-width:0; width:120px" src="#{license_data['image']}" />
+        </a>
+      )
+    end
+
+    html + %(
+      This work is licensed under a <a rel="license" href="#{license_data['id']}"> "#{license_data['term']}" </a>.
+    )
   end
 
   def schema_org_markup
