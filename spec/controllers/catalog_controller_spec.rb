e6786c449afcee8c6fe368191ac75b45449c6cdf
Andy Wallace <andrewwallace@library.ucla.edu>
2026-06-16 16:19:34 -0600

    use sort_title_tsort instead of sort_title_ssort
    minor changes:
    - fix a bug where we sorted solr field 'exists' ascending (no value first) instead of descending (records with a value come first)
    - remove very old solr config - current config is in http://github.com/uclalibrary/calursus-solr
    - auto-accept chromatic changes when running after a merge to main

--- spec/controllers/catalog_controller_spec.rb
+++ spec/controllers/catalog_controller_spec.rb
@@ -232,8 +232,8 @@ RSpec.describe CatalogController, type: :controller do
     let(:expected_sort_fields) do
       [
         "score desc",
-        "exists(sort_title_ssort) asc, sort_title_ssort asc, exists(title_alpha_numeric_ssort) asc, title_alpha_numeric_ssort asc",
-        "exists(sort_title_ssort) asc, sort_title_ssort  desc, exists(title_alpha_numeric_ssort) asc, title_alpha_numeric_ssort desc",
+        "exists(sort_title_tsort) desc, sort_title_tsort asc, exists(title_alpha_numeric_ssort) desc, title_alpha_numeric_ssort asc",
+        "exists(sort_title_tsort) desc, sort_title_tsort  desc, exists(title_alpha_numeric_ssort) desc, title_alpha_numeric_ssort desc",
         "date_dtsort desc",
         "date_dtsort asc",
       ]
